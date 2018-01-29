fdfff5fccd39f7e0a1775549f30c0b33ff57358b
bellachen <bellachen.nyc@gmail.com>
2018-01-29 11:50:17 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewMVC2018_VersionB.Models;
+
+namespace BookReviewMVC2018_VersionB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BookReviewDbEntities db = new BookReviewDbEntities();
+
+            return View(db.Books.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewMVC2018_VersionB.Models;
+
+namespace BookReviewMVC2018_VersionB.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BookReviewDbEntities db = new BookReviewDbEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.Reviewers.ToList());
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "ReviewerUserName,ReviewerFirstName,ReviewerLastName,ReviewerEmail,ReviewPlainPassword")]Reviewer r)
+        {
+            int result = db.usp_NewReviewer(r.ReviewerUserName, r.ReviewerFirstName, r.ReviewerLastName, r.ReviewerEmail, r.ReviewPlainPassword);
+
+            if(result != -1)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
