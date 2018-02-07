81c8ae557832d221545781a15f8fa58553782573
bellachen <bellachen.nyc@gmail.com>
2018-02-07 10:28:54 -0800

    Added login

---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReviewMVC2018_VersionB.Models
+{
+    public class LoginClass
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int ReviewerKey { get; set; }
+    }
+
+}
